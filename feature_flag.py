1b66a7eff399a027d18862a5d8fdcfe6001ae10a
Anton Topchii <anton.topchii@getbyrd.com>
2022-11-22 15:36:34 +0100

    Init commit

---
+++ feature_flag.py
@@ -0,0 +1,19 @@
+class FeatureFlag:
+    def __init__(self, name):
+        self.name = name
+        self.enabled = False
+
+    def __repr__(self):
+        return f"FeatureFlag(name={self.name}, enabled={self.enabled})"
+
+    def __bool__(self):
+        return self.enabled
+
+
+def load_flag(name):
+    ...
+
+
+def flag(name):
+    flag = load_flag(name)
+    return bool(flag)
